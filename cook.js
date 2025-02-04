de6b00303005383d480f4beee3ce08c8290318e7
Jelosus2 <navarrorodriguezjesus6@gmail.com>
2025-02-04 12:23:13 +0100

    add disc drive meta

--- cook.js
+++ cook.js
@@ -1,11 +1,16 @@
+const EquipmentLevelTemplateTb = require("./data/FileCfg/EquipmentLevelTemplateTb.json");
 const WeaponLevelTemplateTb = require("./data/FileCfg/WeaponLevelTemplateTb.json");
 const WeaponStarTemplateTb = require("./data/FileCfg/WeaponStarTemplateTb.json");
 const lib = require("./lib/excels");
 
 if (process.argv.slice(2)[0] == "--excels") {
     const weaponMeta = lib.weaponMeta(WeaponLevelTemplateTb, WeaponStarTemplateTb);
+    const discDriveMeta = lib.discDriveMeta(EquipmentLevelTemplateTb);
     
-    [{ fn: "weaponmeta.json", data: weaponMeta }]
+    [
+        { fn: "weaponmeta.json", data: weaponMeta },
+        { fn: "discdrivemeta.json", data: discDriveMeta }
+    ]
     .forEach((obj) => {
         lib.writeFile(obj.fn, obj.data);
     });
