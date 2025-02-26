6c38f6d112dc93bab9c36e37cac7b620002eb58d
Jelosus2 <navarrorodriguezjesus6@gmail.com>
2025-02-26 18:14:30 +0100

    use new hashing algorithm

--- utils/hash.js
+++ utils/hash.js
@@ -1,14 +1,7 @@
+const XXH = require("xxhashjs");
+
 function stableHash(s = '') {
-  let hash1 = 5381 | 0;
-  let hash2 = hash1 | 0;
-
-  for (let i = 0; i < s.length; i += 2) {
-    hash1 = ((hash1 << 5) + hash1) ^ s.charCodeAt(i);
-    if (i == s.length - 1 || s[i + 1] == '\0') break;
-    hash2 = ((hash2 << 5) + hash2) ^ s.charCodeAt(i + 1);
-  };
-
-  return (hash1 + Math.imul(hash2, 1566083941)) | 0;
+  return XXH.h64(s, 0x0).toString();
 };
 
 exports.GetStableHash = stableHash;
